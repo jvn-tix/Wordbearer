9b9011d094a67804cb7619bdd64c1c7ead43317f
Jvneden <jevoneden21@gmail.com>
2026-09-21 22:14:34 +0700

    adding scene for main delivery and letter collection

---
+++ Assets/Script/LetterData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLetter", menuName = "Postman/Letter Data")]
+public class LetterData : ScriptableObject
+{
+    public string letterID;
+    [TextArea(3, 5)]
+    public string clueText;
+    public string targetHouseID; // Samakan dengan ID Rumah tujuan
+    public Sprite letterSprite;
+}
